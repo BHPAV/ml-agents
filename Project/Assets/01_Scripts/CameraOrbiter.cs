919dcfabf0cf282a304f92f14bd619052e5741bd
BHPAV <120694498+BHPAV@users.noreply.github.com>
2023-01-14 14:14:26 -0500

    MacUpdate and tools
    Added Camera Orbit tool

---
+++ Project/Assets/01_Scripts/CameraOrbiter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraOrbiter : MonoBehaviour
+{
+    public Transform orbitTarget;
+    public float orbitSpeed = 10f;
+
+    void Update()
+    {
+        transform.RotateAround(orbitTarget.position, Vector3.up, orbitSpeed * Time.deltaTime);
+    }
+}
+
